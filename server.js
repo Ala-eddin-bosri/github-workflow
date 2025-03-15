a1ad705140570f8b993db09731f44938c709de04
bosri ala <124356458+Ala-eddin-bosri@users.noreply.github.com>
2025-03-15 03:37:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Hello, GitHub Actions!");
+  res.send("🚀 Hello, from jenkins ");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
